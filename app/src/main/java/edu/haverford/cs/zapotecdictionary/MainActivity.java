8545233307fa39397f2293a375b2e4ab862f07a3
EileenFeng <efeng@brynmawr.edu>
2018-11-01 16:39:08 -0400

    textColor error

--- app/src/main/java/edu/haverford/cs/zapotecdictionary/MainActivity.java
+++ app/src/main/java/edu/haverford/cs/zapotecdictionary/MainActivity.java
@@ -1,13 +1,52 @@
 package edu.haverford.cs.zapotecdictionary;
 
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.shamanland.fonticon.FontIconDrawable;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity  extends FragmentActivity implements
+        ActionBar.TabListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
+    }
+
+    public static class AppSectionPagerAdapter extends FragmentActivity {
+        private Fragment[] frags;
+        SearchFragment tabSearch;
+        HistoryFragment tabHistory;
+        SettingFragment tabSetting;
+
+        public AppSectionPagerAdapter() {
+            tabSearch = new SearchFragment();
+            tabHistory = new HistoryFragment();
+            tabSetting = new SettingFragment();
+
+            frags = new Fragment[] { tabSearch, tabHistory, tabSearch };
+        }
+
+
+
     }
 }
---
+++ app/src/main/java/edu/haverford/cs/zapotecdictionary/SearchFragment.java
@@ -0,0 +1,6 @@
+package edu.haverford.cs.zapotecdictionary;
+
+import android.support.v4.app.ListFragment;
+
+public class SearchFragment extends ListFragment {
+}
