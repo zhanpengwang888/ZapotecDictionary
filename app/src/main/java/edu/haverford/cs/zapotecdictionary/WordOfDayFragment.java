65e202bccfa2a899acbf8f1632e2dcdd79f6d6bb
TarnJu-Han <tarnju-han@TarnJu-HandeMacBook-Air.local>
2018-12-11 14:48:50 -0500

    word of day done

--- app/src/main/java/edu/haverford/cs/zapotecdictionary/WordOfDayFragment.java
+++ app/src/main/java/edu/haverford/cs/zapotecdictionary/WordOfDayFragment.java
@@ -1,20 +1,73 @@
 package edu.haverford.cs.zapotecdictionary;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class WordOfDayFragment extends Fragment {
+    protected static DBHelper mDb;
+    protected static int randomOid;
 
     public WordOfDayFragment() {
         super();
     }
 
+    public void setDB(DBHelper dbHelper){
+        this.mDb = dbHelper;
+    }
+
+    public void set_curID(int newID){
+        this.randomOid = newID;
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saved) {
-        return inflater.inflate(R.layout.word_of_day, container, false);
+        View view = inflater.inflate(R.layout.word_view, container, false);
+        ImageButton imb = view.findViewById(R.id.searchWords_voiceE);
+        imb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = new MediaPlayer();
+                try{
+                    String audiofn = mDb.getInformationFromOID(randomOid, DBHelper.DICTIONARY_COLUMN_AUDIO).toString();
+                    String audiofp = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath()
+                            + "/dataFolder/tlacochahuaya_content/aud/" + audiofn;
+                    mp.setDataSource(audiofp);
+                    mp.prepare();
+                    mp.start();
+                }catch(Exception e){
+                    Toast.makeText(getActivity().getApplicationContext(), "The audio file does not exist.", Toast.LENGTH_SHORT);
+                    e.printStackTrace();
+                }
+            }
+        });
+        TextView word = view.findViewById(R.id.word_WordView);
+        TextView wordEnglishDef = view.findViewById(R.id.word_eng_def);
+        TextView wordEsDef = view.findViewById(R.id.word_es_def);
+        ImageView image = view.findViewById(R.id.word_pic);
+        word.setText(mDb.getInformationFromOID(randomOid, DBHelper.DICTIONARY_COLUMN_LANG).toString());
+        wordEnglishDef.setText(mDb.getInformationFromOID(randomOid, DBHelper.DICTIONARY_COLUMN_GLOSSARY).toString());
+        wordEsDef.setText(mDb.getInformationFromOID(randomOid, DBHelper.DICTIONARY_COLUMN_ES_GLOSS).toString());
+
+        // set picture
+        String wordOfDay_pic = mDb.getInformationFromOID(randomOid, DBHelper.DICTIONARY_COLUMN_IMAGE).toString();
+        if(wordOfDay_pic != null){
+            String wordOfDay_pic_fp = Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DOWNLOADS).getPath() + "/dataFolder/tlacochahuaya_content/pix/" + wordOfDay_pic;
+            Bitmap bMap = BitmapFactory.decodeFile(wordOfDay_pic_fp);
+            image.setImageBitmap(bMap);
+        }
+        return view;
     }
 
     @Override
