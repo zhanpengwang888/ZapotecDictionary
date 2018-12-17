240985f0b87d08c2fc83fc54672aedc5f99c4f1c
zhanpengwang888 <zhanpengwang888@gmail.com>
2018-12-17 15:20:10 -0500

    About fragment added. Not completed yet.

---
+++ app/src/main/java/edu/haverford/cs/zapotecdictionary/AboutFragment.java
@@ -0,0 +1,27 @@
+package edu.haverford.cs.zapotecdictionary;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class AboutFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(savedInstanceState != null) {
+            onViewStateRestored(savedInstanceState);
+        }
+    }
+
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle saved) {
+        View view = inflater.inflate(R.layout.about_page, container, false);
+        TextView documentation = view.findViewById(R.id.documentation);
+        // TODO: waiting for the full documentation of the app from Ju Han
+        // documentation.setText(...);
+        return view;
+    }
+}
